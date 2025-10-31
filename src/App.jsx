52c8080c500d25f424db9e9766660e3073c1ffd2
Akbarkhan <p.akbar96@gmail.com>
2025-10-31 19:34:24 +0530

    UI Changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,10 @@ const [searchTerm, setSearchTerm] = useState('')
 const [taskList, setTaskList] = useState([]);
 
 const AddTask = () => {
+  if(task.Task === '' || task.Status === ''){
+    alert('Please fill all the fields')
+    return
+  }
   if(task.Id == 0){
   setTaskList([...taskList , {Id:Date.now(), Task: task.Task, Status: task.Status}])
   }
@@ -61,13 +65,14 @@ const filteredList = () => {
         setTask={setTask}
         addEvent={AddTask}   
       />
-
+{ taskList.length > 0 &&
       <FormList 
         taskData={filteredList()} 
         EditTask={EditTask}
         DeleteTask={DeleteTask}
         setSearchTerm={FilterSearch}
       />
+}
 
       <Footer />
 
